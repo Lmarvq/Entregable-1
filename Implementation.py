de27a05817b392fddd75889cdc0e797f83996df2
Lmarvq <luisa.martinezv1@udea.edu.co>
2024-03-13 17:15:31 -0500

    Creación y diseño del menú a ejecutar

---
+++ Implementation.py
@@ -0,0 +1,27 @@
+from Clases import *
+
+def main():
+    while True:
+        try:
+            print("¡Bienvenido al Sistema X!\nEscoja la acción a ejecutar.")
+            sis = Sistema()
+            menu = int(input("1-Agregar implante\n2-Editar implante\n3-Eliminar implante\n4-Visualizar el inventario completo"))
+            if menu == 1:
+                sis.IngresarImplante()
+            if menu == 2:
+                sis.EditarImplante()
+            if menu ==3:
+                sis.EliminarImplante()
+            if menu == 4:
+                sis.VisualizarInventario()
+            if menu == 5:
+                op1 = int(input("¿Está seguro que desea salir del sistema?\n1-Si\n2-No"))
+                if op1 == 1:
+                    break
+                if op1 == 2:
+                    continue
+        except: ValueError, TypeError
+        print("Por favor ingrese una de las opciones diseñadas")
+        continue
+        
+main()
